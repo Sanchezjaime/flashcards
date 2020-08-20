04850a0aec32bb1411ac639fcc848fc218e1d001
Jaime Sanchez <sanchezjaime@protonmail.com>
2020-08-19 21:42:05 -0700

    added app.get methods to app.js

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.send('<h1>test test test</h1>');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('<h1>Hello, Javascript Developer!</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('this application is running on localhost:3000!')
+});
