9adbe777f473e3ed07da8d43cf7b0fcbe17a8a86
Jaime Sanchez <sanchezjaime@protonmail.com>
2020-09-02 20:35:25 -0700

    modified cards.js to randomly render flashcard

--- routes/cards.js
+++ routes/cards.js
@@ -3,13 +3,34 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get( '/', ( req, res ) => {
+  const numberOfCards = cards.length;
+  const flashcardsId = Math.floor( Math.random() * numberOfCards );
+  res.redirect( `/cards/${flashcardsId}` )
+});
+
 router.get('/:id', (req, res) => {
   const { side } = req.query;
   const { id } = req.params;
-  const text = cards[id];
+
+  if ( !side ) {
+    res.redirect(`/cards/${id}?side=question`);
+  }
+  const name = req.cookies.username;
+  const text = cards[id][side];
   const { hint } = cards[id];
 
-  const templateData = { text, hint, };
+  const templateData = { id, text, name };
+
+  if (side === 'question') {
+    templateData.hint = hint;
+    templateData.sideToShow = 'answer';
+    templateData.sideToShowDisplay = 'Answer';
+  } else if ( side === 'answer' ) {
+    templateData.sideToShow = 'question';
+    templateData.sideToShowDisplay = 'Question';
+  }
+
   res.render('card', templateData);
 });
 
